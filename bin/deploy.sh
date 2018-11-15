a4ddac26e77ef9a45e90d7a32193a799066d5dcc
Anthony Ettinger <anthony@chovy.com>
2018-11-15 15:24:35 -0800

    add pdfs

---
+++ bin/deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+args=(-azvP --delete --exclude=node_modules --exclude=.idea --exclude=.git)
+hosts=(ylla) # tornado lightning thunder tundra jefferson
+dry=() #add --dry-run to enable testing
+user=ubuntu
+name=ylla
+project=ylla-tokensale
+
+for host in "${hosts[@]}"
+do
+  echo ""
+  date
+  echo "---------------------"
+  echo "syncing ${host}"
+  echo "---------------------"
+  rsync ${dry[@]} ${args[@]} ./ ${user}@${host}:www/${name}/${project}
+  # ssh -t ${user}@${host} \$HOME/www/${name}/${project}/bin/post-deploy.sh
+done
+
+version=$(jq -r .version package.json)
+say "yella token sale is live!"
+exit
---
+++ bin/post-deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# load node
+. $HOME/.bashrc
+. "$NVM_DIR/nvm.sh" && nvm use v11
+
+host=$(hostname)
+name=ylla
+project=ylla-token
+
+. $HOME/www/${name}/${project}/.env.sh
+
+production=$YLLA_PRODUCTION_HOST
+development=$YLLA_DEVELOPMENT_HOST
+
+node -v
+npm -v
+
+echo "current host: $host production: $production development: $development"
+
+if [[ $host == "$production" ]] || [[ $host == "$development" ]]; then
+  cd $HOME/www/${name}/${project}
+  # npm run restart:production
+fi
